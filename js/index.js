dac4b759fdc0b1e3a36814716ec8808de27e6800
Pauli Kostamo <pauli.kostamo@solinor.com>
2016-09-19 15:20:09 +0300

    export more classes

--- js/index.js
+++ js/index.js
@@ -5,4 +5,9 @@ function __export(m) {
 __export(require('./src/PaymentAPI'));
 __export(require('./src/FormBuilder'));
 __export(require('./src/security/SecureSigner'));
+__export(require('./src/model/request/Card'));
+__export(require('./src/model/request/CommitTransactionRequest'));
+__export(require('./src/model/request/RevertTransactionRequest'));
+__export(require('./src/model/request/TransactionRequest'));
+__export(require('./src/model/Token'));
 //# sourceMappingURL=index.js.map
