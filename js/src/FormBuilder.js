803d951e4fad8723a86899f82ada4cc31dd8ab2b
Pauli Kostamo <pauli.kostamo@solinor.com>
2018-02-28 17:29:57 +0200

    JS

--- js/src/FormBuilder.js
+++ js/src/FormBuilder.js
@@ -334,11 +334,11 @@ class FormBuilder {
      * @param failureUrl             The URL the user is redirected after a failure such as an authentication or connectivity error.
      * @param cancelUrl              The URL the user is redirected after cancelling the transaction (clicking on the cancel button).
      * @param language               The language the form is displayed in.
-     * @param amount                 The amount to pay.
+     * @param amount                 The amount to pay in euro cents. Pivo supports only euros.
      * @param orderId                A generated order ID, may for example be always unique or used multiple times for recurring transactions.
      * @param description            Description of the payment shown in the form.
      * @param phoneNumber            User phone number with country code. Max AN 15. Optional.
-     * @param referenceNumber        Reference number for payment. Optional.
+     * @param reference              Reference for payment. Optional.
      * @param appUrl                 When used, Pivo tries to open application with this url. Optional.
      * @param skipFormNotifications  Skip notifications displayed on the Payment Highway form. May be null.
      * @param exitIframeOnResult     Exit from iframe after a result. May be null.
@@ -348,7 +348,7 @@ class FormBuilder {
      * @param webhookDelay           Delay for webhook in seconds. Between 0-900
      * @return FormContainer
      */
-    generatePivoParameters(successUrl, failureUrl, cancelUrl, language, amount, orderId, description, phoneNumber, referenceNumber, appUrl, getPivoUrl, skipFormNotifications, exitIframeOnResult, webhookSuccessUrl, webhookFailureUrl, webhookCancelUrl, webhookDelay) {
+    generatePivoParameters(successUrl, failureUrl, cancelUrl, language, amount, orderId, description, phoneNumber, reference, appUrl, skipFormNotifications, exitIframeOnResult, webhookSuccessUrl, webhookFailureUrl, webhookCancelUrl, webhookDelay) {
         const requestId = PaymentHighwayUtility_1.PaymentHighwayUtility.createRequestId();
         let nameValuePairs = this.createCommonNameValuePairs(successUrl, failureUrl, cancelUrl, language, requestId);
         nameValuePairs.push(new Pair_1.Pair(FormBuilder.SPH_AMOUNT, amount.toString()));
@@ -364,8 +364,8 @@ class FormBuilder {
         if (typeof phoneNumber !== 'undefined') {
             nameValuePairs.push(new Pair_1.Pair(FormBuilder.SPH_PHONE_NUMBER, phoneNumber));
         }
-        if (typeof referenceNumber !== 'undefined') {
-            nameValuePairs.push(new Pair_1.Pair(FormBuilder.SPH_REFERENCE_NUMBER, referenceNumber));
+        if (typeof reference !== 'undefined') {
+            nameValuePairs.push(new Pair_1.Pair(FormBuilder.SPH_REFERENCE, reference));
         }
         if (typeof appUrl !== 'undefined') {
             nameValuePairs.push(new Pair_1.Pair(FormBuilder.SPH_APP_URL, appUrl));
@@ -463,7 +463,7 @@ FormBuilder.SPH_SHOW_PAYMENT_METHOD_SELECTOR = 'sph-show-payment-method-selector
 FormBuilder.SPH_REQUEST_SHIPPING_ADDRESS = 'sph-request-shipping-address';
 FormBuilder.SPH_PHONE_NUMBER = 'sph-phone-number';
 FormBuilder.SPH_APP_URL = 'sph-app-url';
-FormBuilder.SPH_REFERENCE_NUMBER = 'sph-reference-number';
+FormBuilder.SPH_REFERENCE = 'sph-reference';
 FormBuilder.LANGUAGE = 'language';
 FormBuilder.DESCRIPTION = 'description';
 FormBuilder.SIGNATURE = 'signature';
