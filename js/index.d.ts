7f63ff3b369dd27ff3053162726fed70d02eb548
Pauli Kostamo <pauli.kostamo@solinor.com>
2018-05-02 17:29:07 +0300

    JS

--- js/index.d.ts
+++ js/index.d.ts
@@ -4,5 +4,6 @@ export * from './src/security/SecureSigner';
 export * from './src/model/request/Card';
 export * from './src/model/request/CommitTransactionRequest';
 export * from './src/model/request/RevertTransactionRequest';
+export * from './src/model/request/RevertSiirtoTransactionRequest';
 export * from './src/model/request/TransactionRequest';
 export * from './src/model/Token';
