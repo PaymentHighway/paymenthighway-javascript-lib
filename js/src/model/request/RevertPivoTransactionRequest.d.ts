4b5d791d1f1bcdec6bc55e4bc3ba24ed2810448d
Pauli Kostamo <pauli.kostamo@solinor.com>
2018-05-04 15:27:46 +0300

    JS

---
+++ js/src/model/request/RevertPivoTransactionRequest.d.ts
@@ -0,0 +1,5 @@
+export declare class RevertPivoTransactionRequest {
+    reference_number: string;
+    amount: number;
+    constructor(reference_number?: string, amount?: number);
+}
---
+++ js/src/model/request/RevertPivoTransactionRequest.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class RevertPivoTransactionRequest {
+    constructor(reference_number, amount) {
+        this.reference_number = reference_number;
+        this.amount = amount;
+    }
+}
+exports.RevertPivoTransactionRequest = RevertPivoTransactionRequest;
+//# sourceMappingURL=RevertPivoTransactionRequest.js.map
