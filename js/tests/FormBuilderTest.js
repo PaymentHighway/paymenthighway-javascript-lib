fc0d0e416647439ff96cfc7b320f18542b2903d3
Margus Sipria <margus.sipria@paymenthighway.fi>
2020-02-25 13:49:59 +0200

    JS

--- js/tests/FormBuilderTest.js
+++ js/tests/FormBuilderTest.js
@@ -333,27 +333,6 @@ describe('Form builder', () => {
             chai_1.assert.match(response.headers.location, /\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\/masterpass/, 'redirect location doesn\'t match ' + response.header);
         });
     });
-    it('Test siirto form with mandatory parameters', () => {
-        const referenceNumber = '1313';
-        const formContainer = formBuilder.generateSiirtoParameters(successUrl, failureUrl, cancelUrl, language, amount, orderId, description, referenceNumber);
-        testNameValuePairs(formContainer.nameValuePairs, 15);
-        return FormConnection_1.FormConnection.postForm(formContainer)
-            .then((response) => {
-            chai_1.assert(response.statusCode === 303, 'Response status code should be 303, got ' + response.statusCode);
-            chai_1.assert.match(response.headers.location, /\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\/siirto/, 'redirect location doesn\'t match ' + response.header);
-        });
-    });
-    it('Test siirto form with optional parameters', () => {
-        const phoneNumber = '+358441234567';
-        const referenceNumber = '1313';
-        const formContainer = formBuilder.generateSiirtoParameters(successUrl, failureUrl, cancelUrl, language, amount, orderId, description, referenceNumber, phoneNumber);
-        testNameValuePairs(formContainer.nameValuePairs, 16);
-        return FormConnection_1.FormConnection.postForm(formContainer)
-            .then((response) => {
-            chai_1.assert(response.statusCode === 303, 'Response status code should be 303, got ' + response.statusCode);
-            chai_1.assert.match(response.headers.location, /\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\/siirto/, 'redirect location doesn\'t match ' + response.header);
-        });
-    });
     it('Test add card webhook parameters', () => {
         const formContainer = formBuilder.generateAddCardParameters(successUrl, failureUrl, cancelUrl, language, undefined, undefined, undefined, undefined, undefined, webhookSuccessUrl, webhookFailureUrl, webhookCancelUrl, webhookDelay);
         testNameValuePairs(formContainer.nameValuePairs, 14);
